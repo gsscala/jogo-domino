60b78e19ef755c515de624097f425a017b954177
Giovanni <160168579+gsscala@users.noreply.github.com>
2024-07-02 18:52:12 -0300

    Update integrante_2.py

--- src/integrante_2.py
+++ src/integrante_2.py
@@ -1,62 +1,52 @@
 from basic_players import Player
 import random
 
-
-
-
-class jovane(Player):
+class giovanni(Player):
 
     def __init__(self):
-        super().__init__(281210, "cebola")
+        super().__init__(281210, "giovanni", image_path="img/giovanni.jpeg")
 
     def info(self, historico, posicao, extremos):
-        # ver que na real eu posso contabilizar as peças que ja foram jogadas com as que eu tenho para descobrir quais peças ninguem alem de mim tem
-        global extremidades
-        global pecas
         extremidades = []
-        for peca in [
-            peca_[1] for peca_ in historico if not peca_[3] and peca_[0] % 2 != posicao % 2
+        for extremidade in [
+            peca[1] for peca in historico if not peca[3] and peca[0] % 2 != posicao % 2
         ]:
-            extremidades.append(peca[0][0])
-            extremidades.append(peca[1][1])
+            extremidades.append(extremidade[0])
+            extremidades.append(extremidade[1])
         jogadas_bloqueadoras = []
         for peca in self._tiles:
             for extremidade in extremidades:
                 if extremidade == peca[0]:
                     if peca[1] == extremos[0]:
-                        jogadas_bloqueadoras.append(0, peca)
+                        jogadas_bloqueadoras.append((0, peca))
                     if peca[1] == extremos[1]:
-                        jogadas_bloqueadoras.append(1, (peca[1], peca[0]))
+                        jogadas_bloqueadoras.append((1, peca))
                 if extremidade == peca[1]:
                     if peca[0] == extremos[0]:
-                        jogadas_bloqueadoras.append(0, (peca[1], peca[0]))
+                        jogadas_bloqueadoras.append((0, peca))
                     if peca[0] == extremos[1]:
-                        jogadas_bloqueadoras.append(1, peca)
-        pecas = sorted([peca for peca in self._tiles if peca[0] == peca[1]], key = lambda x: (x[0] + x[1]), reverse=True) + sorted(self._tiles, key = lambda x: (x[0] + x[1]), reverse=True)
-        for peca in pecas:
-            if extremos[0] == peca[0]:
-                peca = 0, (peca[1], peca[0])
-            elif extremos[0] == peca[1]:
-                peca = 0, peca
-            elif extremos[1] == peca[0]:
-                peca = 1, peca
-            elif extremos[1] == peca[1]:
-                peca = 1, (peca[1], peca[0])
-            else:
-                pecas.remove(peca)
+                        jogadas_bloqueadoras.append((1, peca))
+        pecas = []
+        for peca in self._tiles:
+            if extremos[0] in peca:
+                pecas.append((0, peca))
+            elif extremos[1] in peca:
+                pecas.append((1, peca))
+        pecas = sorted([peca for peca in pecas if peca[0] == peca[1]], key = lambda x: (x[1][0] + x[1][1]), reverse=True) + sorted(pecas, key = lambda x: (x[1][0] + x[1][1]), reverse=True)
         pecas = sorted(jogadas_bloqueadoras, key = lambda x: (x[1][0] + x[1][1]), reverse = True) + pecas
+        return pecas, extremidades
 
     def play(self, board_extremes, play_hist):
         if len(play_hist) == 0:
-            return random.randint(0, 1), (sorted([peca for peca in self._tiles if peca[0] == peca[1]], key = lambda x: (x[0] + x[1]), reverse=True) + sorted(self._tiles, key = lambda x: (x[0] + x[1]), reverse=True))[0] 
-        jovane.info(self, play_hist, jovane.position, board_extremes)
-        print(pecas)
-        if board_extremes[0] in extremidades:
-            for jogada in pecas: #colocar isso no começo da lista de peças
-                if jogada[0] == 1:
-                    return jogada # se nao tiver nada jogar a melhor na direita
-        if board_extremes[1] in extremidades:
-            for jogada in pecas: #colocar isso no começo da lista de peças
-                if jogada[0] == 0:
-                    return jogada # se nao tiver nada jogar a melhor na esquerda
-        return pecas[0]
+            pecas = sorted([peca for peca in self._tiles if peca[0] == peca[1]], key = lambda x: (x[0] + x[1]), reverse=True) + sorted(self._tiles, key = lambda x: (x[0] + x[1]), reverse=True)
+            return random.randint(0, 1), pecas[0]
+        pecas, extremidades = self.info(play_hist, self._position, board_extremes)
+        for peca in pecas:
+            if board_extremes[0] in extremidades and peca[0] == 1:
+                return peca
+            if board_extremes[1] in extremidades and peca[0] == 0:
+                return peca
+        for peca in pecas:
+            if set(board_extremes) & set(peca[1]):
+                return peca
+        return 1, None
